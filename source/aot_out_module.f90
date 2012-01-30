8337389fa4533fab6680f4ba281dd15e44ceeacf
Harald Klimach <harald@klimachs.de>
2012-01-30 16:47:07 +0100

    Do not use MPI in Aotus. Aotus does not depend on MPI up to now, and I do not see a compelling reason to introduce it.

--- source/aot_out_module.f90
+++ source/aot_out_module.f90
@@ -1,15 +1,24 @@
 !> First draft of incomplete output module!
+!!
+!! This module eases the output of readable Lua scripts.
+!! It takes care of indentation with nested tables, and
+!! provides a concise interface to output Fortran data
+!! into Lua scripts.
+!! Therefore this module is somehow the counter-part to
+!! the reading functions, however it is almost completely
+!! independent and relies purely on Fortran output methods.
 module aot_out_module
-  use mpi
 
   implicit none
 
-  type put_conf_type
+  public :: aot_out_type
+  public :: aot_put_val
+
+  type aot_out_type
     integer :: outunit
     integer :: indent
     integer :: stack(100)
     integer :: level
-    integer :: comm
   end type 
 
   interface aot_put_val
@@ -18,31 +27,33 @@ module aot_out_module
     module procedure aot_put_val_real
   end interface
 
-  contains
+  private
+
+  integer, parameter :: labelLen = 80
+
+contains
 
 !******************************************************************************!
 !> open the file, set the filename and the values of put_conf
 !!
-  subroutine aot_open_put(filename, put_conf, comm)
+!! Will overwrite the given file, if it already exists.
+  subroutine aot_open_put(put_conf, filename)
     !------------------------------------------------------------------------ 
-    character(len=labelLen), intent(in) :: filename
-    type(put_conf_type), intent(inout) :: put_conf
-    integer, intent(in) :: comm
+    character(len=*), intent(in) :: filename
+    type(aot_out_type), intent(out) :: put_conf
     !------------------------------------------------------------------------ 
     ! defining local variables
     integer :: rank
     integer :: iError
     !------------------------------------------------------------------------ 
 
-    put_conf%comm = comm
-    call MPI_COMM_RANK(comm, rank, iError) 
-    if (rank .eq. 0) then
-      put_conf%outunit = newunit()
-      open (unit = put_conf%outunit, file = trim(filename))
-      put_conf%indent = 0
-      put_conf%stack(:) = 0 
-      put_conf%level = 0
-    end if
+    put_conf%outunit = newunit()
+    open(unit = put_conf%outunit, file = trim(filename), action = 'write', &
+      &  status='replace', recl=360)
+    put_conf%indent = 0
+    put_conf%stack(:) = 0 
+    put_conf%level = 0
+
   end subroutine aot_open_put
 !******************************************************************************!
 
@@ -52,12 +63,12 @@ module aot_out_module
 !!
   subroutine aot_table_open_out(put_conf, tname)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
-    character(len=labelLen), intent(in) :: tname
+    type(aot_out_type), intent(inout)  :: put_conf
+    character(len=*), intent(in) :: tname
     !------------------------------------------------------------------------ 
     
     if (present(tname)) then
-      write(put_conf%outunit,*) 'Initial Conditions = {' 
+      write(put_conf%outunit,*) trim(tname)//' = {' 
     else
       write(put_conf%outunit,*) '{'
     end if
@@ -72,7 +83,7 @@ module aot_out_module
 !!
   subroutine aot_table_close_out(put_conf)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
+    type(aot_out_type), intent(inout)  :: put_conf
     !------------------------------------------------------------------------ 
     put_conf%indent = put_conf%indent - 4
     put_conf%level = put_conf%level - 1
@@ -86,7 +97,7 @@ module aot_out_module
 !!
   subroutine aot_close_put(put_conf)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
+    type(aot_out_type), intent(inout)  :: put_conf
     !------------------------------------------------------------------------ 
     close( put_conf%outunit )
   end subroutine aot_close_put
@@ -98,8 +109,8 @@ module aot_out_module
 !!
   subroutine aot_put_val_int(put_conf, vname, val)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
-    character(len=labelLen), intent(in) :: vname
+    type(aot_out_type), intent(inout)  :: put_conf
+    character(len=*), intent(in) :: vname
     integer, intent(in) :: val
     !------------------------------------------------------------------------ 
     if ( put_conf%level .gt. 0 ) then
@@ -119,8 +130,6 @@ module aot_out_module
     else
       if(level .ne. 0) then
         write(put_conf%outunit,*, advance ='no') val
-      else
-        call tem_write('Plain values shall onlz be used for tables!!!!!')
       end if 
     end if
   end subroutine aot_put_val_int
@@ -132,8 +141,8 @@ module aot_out_module
 !!
   subroutine aot_put_val_char(put_conf, vname, val)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
-    character(len=labelLen), optional, intent(in) :: vname
+    type(aot_out_type), intent(inout)  :: put_conf
+    character(len=*), optional, intent(in) :: vname
     character, intent(in) :: val
     !------------------------------------------------------------------------ 
     if ( put_conf%level .gt. 0 ) then
@@ -153,8 +162,6 @@ module aot_out_module
     else
       if(level .ne. 0) then
         write(put_conf%outunit,*, advance ='no') val
-      else
-        call tem_write('Plain values shall onlz be used for tables!!!!!')
       end if 
     end if
   end subroutine aot_put_val_char
@@ -166,8 +173,8 @@ module aot_out_module
 !!
   subroutine aot_put_val_real(put_conf, vname, val)
     !------------------------------------------------------------------------ 
-    type(put_conf_type), intent(inout)  :: put_conf
-    character(len=labelLen), intent(in) :: vname
+    type(aot_out_type), intent(inout)  :: put_conf
+    character(len=*), intent(in) :: vname
     real, intent(in) :: val
     !------------------------------------------------------------------------ 
     if ( put_conf%level .gt. 0 ) then
@@ -187,11 +194,27 @@ module aot_out_module
     else
       if(level .ne. 0) then
         write(put_conf%outunit,*, advance ='no') val
-      else
-        call tem_write('Plain values shall onlz be used for tables!!!!!')
       end if 
     end if
   end subroutine aot_put_val_real
+ 
 
+  !> Helper function to provide new unit, as long as F2008 newunit argument
+  !! in open statement is not commonly available.
+  !! To be used right in front of the open statement like this:
+  !!  myUnit = newunit()
+  !!  open(myUnit, ...)
+  function newunit() result(nu)
+    integer :: nu
+
+    logical :: connected
+
+    nu = 21
+    inquire(unit=nu, opened=connected)
+    do while(connected)
+      nu = nu + 1
+      inquire(unit=nu, opened=connected)
+    end do
+  end function newunit
 
 end module aot_out_module
